0ed0c167783d4e18afc64240e66ac92bfc6f75ca
Greeshma <greeshmahere@gmail.com>
2018-01-26 17:51:00 -0600

    Initial Commit

---
+++ src/main/java/com/project/Organisation/OrganisationApplication.java
@@ -0,0 +1,12 @@
+package com.project.Organisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganisationApplication.class, args);
+	}
+}
