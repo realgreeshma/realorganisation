d7bdfe12b42ed8c21a2d7bfdd125923b871b7f87
Greeshma <greeshmahere@gmail.com>
2018-01-26 18:15:20 -0600

    Added a Bean name Organisation in a package of domain.

---
+++ src/main/java/domain/Organisation.java
@@ -0,0 +1,37 @@
+package domain;
+
+public class Organisation {
+    private long id;
+    private String nameoforganisation;
+    private String requirementsoforganisation;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNameoforganisation() {
+        return nameoforganisation;
+    }
+
+    public String getRequirementsoforganisation() {
+        return requirementsoforganisation;
+    }
+
+    @Override
+    public String toString() {
+        return "Organisation{" +
+                "id=" + id +
+                ", nameoforganisation='" + nameoforganisation + '\'' +
+                ", requirementsoforganisation='" + requirementsoforganisation + '\'' +
+                '}';
+    }
+
+    public Organisation(long id, String nameoforganisation, String requirementsoforganisation) {
+        this.id = id;
+        this.nameoforganisation = nameoforganisation;
+        this.requirementsoforganisation = requirementsoforganisation;
+
+
+
+    }
+}
